f9da6a5c5581af2001effcead1433e25577856ad
Park <pjw9195@naver.com>
2019-04-14 02:16:01 +0900

    prop 활용

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './movie';
 
-const movies = ['Matrix', 'Full Metal Jacket', 'Oldboy', 'Star Wars'];
+const movieTitles = ['Matrix', 'Full Metal Jacket', 'Oldboy', 'Star Wars'];
 
+const movieImages = [
+  'http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg',
+  'http://image.yes24.com/momo/TopCate1936/MidCate002/193514261.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zD87lp1lNjdI5zP71yXMNOKU0e39yUveuQuEU4l8MJeclXgP',
+  'https://vignette.wikia.nocookie.net/starwars/images/1/12/TheLastJediTheatricalPoster.jpg/revision/latest?cb=20171014042219&path-prefix=ko',
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movies[0]} />
-        <Movie title={movies[1]} />
-        <Movie title={movies[2]} />
-        <Movie title={movies[3]} />
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
--- src/movie.js
+++ src/movie.js
@@ -6,19 +6,15 @@ class Movie extends Component {
     console.log(this.props);
     return (
       <div>
-        <h1>hello this is a movie</h1>
+        <MoviePoster poster={this.props.poster} />
+        <h1>{this.props.title}</h1>
       </div>
     );
   }
 }
 class MoviePoster extends Component {
   render() {
-    return (
-      <img
-        src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F9%2F9d%2FMockingjay_Part_2_Poster.jpg%2F220px-Mockingjay_Part_2_Poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FThe_Hunger_Games%3A_Mockingjay_%25E2%2580%2593_Part_2&docid=e8YHqTYbm6vQ3M&tbnid=--UnAmkj-OBMJM%3A&vet=10ahUKEwirvf3b0qLhAhUwE6YKHaPRAnQQMwg6KAQwBA..i&w=220&h=334&bih=744&biw=767&q=hungergame&ved=0ahUKEwirvf3b0qLhAhUwE6YKHaPRAnQQMwg6KAQwBA&iact=mrc&uact=8"
-        alt=""
-      />
-    );
+    return <img src={this.props.poster} alt="" />;
   }
 }
 export default Movie;
