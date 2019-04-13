0061de5f14970bc39321c40c82c7e5ee77a9d8a0
Park <pjw9195@naver.com>
2019-04-14 02:57:29 +0900

    List => map

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './movie';
 
-const movieTitles = ['Matrix', 'Full Metal Jacket', 'Oldboy', 'Star Wars'];
-
-const movieImages = [
-  'http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg',
-  'http://image.yes24.com/momo/TopCate1936/MidCate002/193514261.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zD87lp1lNjdI5zP71yXMNOKU0e39yUveuQuEU4l8MJeclXgP',
-  'https://vignette.wikia.nocookie.net/starwars/images/1/12/TheLastJediTheatricalPoster.jpg/revision/latest?cb=20171014042219&path-prefix=ko',
+const movies = [
+  {
+    title: 'Matrix',
+    poster: 'http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg',
+  },
+  {
+    title: 'Full Metal Jacket',
+    poster: 'http://image.yes24.com/momo/TopCate1936/MidCate002/193514261.jpg',
+  },
+  {
+    title: 'Oldboy',
+    poster:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3zD87lp1lNjdI5zP71yXMNOKU0e39yUveuQuEU4l8MJeclXgP',
+  },
+  {
+    title: 'Star Wars',
+    poster:
+      'https://vignette.wikia.nocookie.net/starwars/images/1/12/TheLastJediTheatricalPoster.jpg/revision/latest?cb=20171014042219&path-prefix=ko',
+  },
 ];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {movies.map(movie => (
+          <Movie title={movie.title} poster={movie.poster} />
+        ))}
       </div>
     );
   }
