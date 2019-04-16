70ee0857a0598143aa0e4907d64b00e5bc02f97e
Park <pjw9195@naver.com>
2019-04-16 10:13:44 +0900

    json 을 이용해서 영화 image와 title을 출력

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import Movie from './movie';
 class App extends Component {
   state = {};
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
+    this._getMovies();
   }
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => (
-      <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => (
+      <Movie
+        title={movie.title}
+        poster={movie.large_cover_image}
+        key={movie.id}
+      />
     ));
     return movies;
+  };
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies,
+    });
+  };
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
   render() {
     return (
